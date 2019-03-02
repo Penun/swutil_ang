9a15d19b08e82606ae901a48788db804217cfbca
Damon <damon.frankfurth@gmail.com>
2019-03-02 08:40:23 -0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/Penun/swutil_ang/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/Penun/swutil_ang/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
